5906a0e82f241bd19dfcc870a06d77953b7b5c93
deepakkumar1211 <sahudeepak1211@gmail.com>
2025-06-03 14:38:37 +0530

    added start scripts

--- models/User.js
+++ models/User.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
-    username: { type: String, required: true },
+    username: { type: String, required: true, unique: true },
     email: { type: String, unique: true, required: true },
     password: { type: String, required: true },
 });
